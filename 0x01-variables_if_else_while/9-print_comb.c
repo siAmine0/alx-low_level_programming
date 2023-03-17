2089c942f1546b5a76fc475688fe5dfaf7f59d48
Amine <samiensa1505@gmail.com>
2023-03-17 02:56:58 -0700

    c 2

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,14 +6,17 @@
  */
 int main(void)
 {
-int n;
-for (n = 48; n < 58; n++){
-	putchar(n);
-	if (n != 57){
-	putchar(',');
-	putchar(' ');
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
