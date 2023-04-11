c5b432a7632f216ebb4b327590e8464e43544a5c
sami <samiensa1505@gmail.com>
2023-04-11 06:11:33 -0700

    malloc_free

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - check the code for ALX School students.
  *
@@ -9,15 +8,15 @@
  */
 int main(void)
 {
-    char *s;
+	char *s;
 
-    s = str_concat("Betty ", "Holberton");
-    if (s == NULL)
-    {
-        printf("failed\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
+	s = str_concat("Betty ", "Holberton");
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
 }
